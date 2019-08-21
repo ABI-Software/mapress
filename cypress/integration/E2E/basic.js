0cad155ee987332d75d10ce1c4381677c630093e
alan-wu <alan.wu@auckland.ac.nz>
2019-08-22 11:49:27 +1200

    First commit.

---
+++ cypress/integration/E2E/basic.js
@@ -0,0 +1,13 @@
+describe('Default visit', function() {
+    it('Successfully loads', function() {
+        cy.visit('/');
+
+        cy.get("#mapcore_search_results_container").find(".search-result").should('have.length', 5);
+
+        cy.get("#mapcore_search_input").type('heart').should('have.value', 'heart');
+
+     //   cy.get(".search-button.el-button.mapcore-search-button").click();
+
+     //   cy.get("#mapcore_search_results_container").find(".search-result").should('have.length', 4);
+    })
+})
